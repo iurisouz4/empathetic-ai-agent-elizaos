13a17632b1082bb40e15cdec708168c8bea70f19
Iuri Souza <iuri.santanna@g.globo>
2025-11-27 19:08:43 -0300

    feat: crisisReport load

--- src/plugins/mental-health/index.ts
+++ src/plugins/mental-health/index.ts
@@ -1,10 +1,13 @@
 import { type Plugin } from '@elizaos/core';
 import { crisisAction } from './crisisAction';
+import { crisisReportAction } from './crisisReportAction';
+import * as schema from '../../db/schema';
 
 const mentalHealthPlugin: Plugin = {
   name: 'mental-health-plugin',
   description: 'Ações para saúde mental infanto juvenil.',
-  actions: [crisisAction],
+  actions: [crisisAction, crisisReportAction],
+  schema,
 };
 
 export default mentalHealthPlugin;
